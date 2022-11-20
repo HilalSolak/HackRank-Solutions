c417aa4fb20a3750a4bebf0e40d35df9e5d9c090
Hilal solak <56636066+HilalSolak@users.noreply.github.com>
2022-11-21 02:24:01 +0300

    Add files via upload
    Given n  positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers. In example:
    arr=[1,3,5,7,9]
    minsum=1+3+5+7=16
    maxsum=3+5+7+9=24
    output:
    16 24

---
+++ 1 week preparation kit/2)Mini-MaxSum.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        Collections.sort(arr);
+        long sum=0;
+        int n=arr.size();
+        int minnumber=arr.get(0);
+        int maxnumber=arr.get(n-1);
+        for(int i=0;i<arr.size();i++){
+             sum+=arr.get(i);
+         }
+         System.out.println((sum-maxnumber)+" "+(sum-minnumber));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
