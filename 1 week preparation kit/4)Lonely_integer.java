dc81416b05e4d126028b912a08573b46a5195eb5
Hilal solak <56636066+HilalSolak@users.noreply.github.com>
2022-11-22 00:46:55 +0300

    Add files via upload
    Given an array of integers, where all elements but one occur twice, find the unique element.

---
+++ 1 week preparation kit/4)Lonely_integer.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+        Collections.sort(a);
+    for(int i=0; i<a.size(); i=i+2){
+        if(i==a.size()-1 || a.get(i) != a.get(i+1)){
+            return a.get(i);
+        }
+    }
+      return -1;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
